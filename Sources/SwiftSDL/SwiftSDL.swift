2a75d7f443b83c36f2723193eaead72d370ace2a
Rick Mann <rmann@latencyzero.com>
2019-08-26 02:50:26 -0700

    .

--- Sources/SwiftSDL/SwiftSDL.swift
+++ Sources/SwiftSDL/SwiftSDL.swift
@@ -8,9 +8,9 @@ SDLInitFlags : OptionSet
 {
 	public let		rawValue		:	Int
 	
-	static let timer				=	SDLInitFlags(rawValue: 0x00000001)
-	static let video				=	SDLInitFlags(rawValue: 0x00000020)
-	static let events				=	SDLInitFlags(rawValue: 0x00004000)
+	public static let timer					=	SDLInitFlags(rawValue: 0x00000001)
+	public static let video					=	SDLInitFlags(rawValue: 0x00000020)
+	public static let events				=	SDLInitFlags(rawValue: 0x00004000)
 	
 	public
 	init(rawValue inVal: Int)
