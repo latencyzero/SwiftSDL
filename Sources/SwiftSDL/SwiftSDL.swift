06c8103d2f738934d3fbd0231c0f692dec0acc93
Rick Mann <rmann@latencyzero.com>
2019-08-26 02:50:00 -0700

    .

--- Sources/SwiftSDL/SwiftSDL.swift
+++ Sources/SwiftSDL/SwiftSDL.swift
@@ -1,6 +1,33 @@
-struct SwiftSDL {
-    var text = "Hello, World!"
+import CSDL2
+
+
+
+public
+struct
+SDLInitFlags : OptionSet
+{
+	public let		rawValue		:	Int
+	
+	static let timer				=	SDLInitFlags(rawValue: 0x00000001)
+	static let video				=	SDLInitFlags(rawValue: 0x00000020)
+	static let events				=	SDLInitFlags(rawValue: 0x00004000)
+	
+	public
+	init(rawValue inVal: Int)
+	{
+		self.rawValue = inVal
+	}
 }
 
+public
+func
+SDLInit(flags inFlags: SDLInitFlags)
+{
+}
 
-
+public
+func
+SDLQuit()
+{
+	SDL_Quit()
+}
