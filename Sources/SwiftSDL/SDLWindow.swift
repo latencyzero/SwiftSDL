aa5930a94f47efa2fe1535012a5f5ff1ba0332ba
Rick Mann <rmann@latencyzero.com>
2019-08-26 20:52:55 -0700

    Problem with SDL_Window

--- Sources/SwiftSDL/SDLWindow.swift
+++ Sources/SwiftSDL/SDLWindow.swift
@@ -4,9 +4,36 @@ import CSDL2
 
 
 
+public
+struct
+SDLCreateWindowFlags : OptionSet
+{
+	public let	rawValue			:	UInt32
+	
+	public static let	fullscreen						=	SDLCreateWindowFlags(rawValue: SDL_WINDOW_FULLSCREEN.rawValue)
+	public static let	openGL							=	SDLCreateWindowFlags(rawValue: SDL_WINDOW_OPENGL.rawValue)
+	public static let	shown							=	SDLCreateWindowFlags(rawValue: SDL_WINDOW_SHOWN.rawValue)
+	public static let	hidden							=	SDLCreateWindowFlags(rawValue: SDL_WINDOW_HIDDEN.rawValue)
+	public static let	borderless						=	SDLCreateWindowFlags(rawValue: SDL_WINDOW_BORDERLESS.rawValue)
+	public static let	resizable						=	SDLCreateWindowFlags(rawValue: SDL_WINDOW_RESIZABLE.rawValue)
+	
+	public
+	init(rawValue inValue: UInt32)
+	{
+		self.rawValue = inValue
+	}
+}
 
 public
 class
 SDLWindow
 {
+	public
+	init(title inTitle: String, width inWidth: Int, height inHeight: Int)
+	{
+		self.window = SDL_CreateWindow(inTitle, 0x1FFF0000, 0x1FFF0000, inWidth, inHeight, 0)
+	}
+	
+	
+	let			window				:	SDL_Window
 }
--- Sources/SwiftSDL/SwiftSDL.swift
+++ Sources/SwiftSDL/SwiftSDL.swift
@@ -8,9 +8,15 @@ SDLInitFlags : OptionSet
 {
 	public let		rawValue		:	UInt32
 	
-	public static let timer					=	SDLInitFlags(rawValue: SDL_INIT_TIMER)
-	public static let video					=	SDLInitFlags(rawValue: 0x00000020)
-	public static let events				=	SDLInitFlags(rawValue: 0x00004000)
+	public static let timer					=	SDLInitFlags(rawValue: 1 << 0)
+	public static let audio					=	SDLInitFlags(rawValue: 1 << 4)
+	public static let video					=	SDLInitFlags(rawValue: 1 << 5)
+	public static let joystick				=	SDLInitFlags(rawValue: 1 << 9)
+	public static let haptic				=	SDLInitFlags(rawValue: 1 << 12)
+	public static let gameController		=	SDLInitFlags(rawValue: 1 << 14)
+	public static let events				=	SDLInitFlags(rawValue: 1 << 14)
+	public static let sensor				=	SDLInitFlags(rawValue: 1 << 15)
+	public static let everything			=	SDLInitFlags([.timer, .audio, .video, .events, .joystick, .haptic, .gameController, .sensor])
 	
 	public
 	init(rawValue inVal: UInt32)
